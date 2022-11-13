e466579a1abedba54f8d0763abbdcde7674899b1
seojinvely1004 <seojin0124@naver.com>
2022-11-13 14:12:46 +0900

    feat: result vs shareResult || 클래스변수 vs인스턴스변수

--- java baseball/baseball/src/main/java/baseball/domain/baseball/Application.java
+++ java baseball/baseball/src/main/java/baseball/domain/baseball/Application.java
@@ -4,11 +4,23 @@ import baseball.domain.Calculator;
 
 public class Application {
     public static void main(String[] args) {
-        Calculator teacher = new Calculator();
-        System.out.println(teacher.add(1,3));
+        Calculator teacher = new Calculator(); //teacher라는 인스턴스
         
-   
+        System.out.println(teacher.add(1,3));  //4
+        //System.out.println(teacher.shareResult); //4
+        System.out.println(Calculator.shareResult);
+
+        Calculator calculator = new Calculator(); //calculator라는 인스턴스
         
+        System.out.println(calculator.add(2000,21); //2021  
+        //System.out.println(calculator.shareResult); //2021
+        System.out.println(Calculator.shareResult);//2021
+
+        System.out.println(teacher.result);//4 //이미 인스턴스화 할 때 복사가 되었기 때문에 
+        //System.out.println(teacher.shareResult);//2021
+        System.out.println(Calculator.shareResult);//2021
+
+        //t도 c도 add라는 기능을 제공하지만 복사본은 그 값(result = 0;)을 복사해오는 것이지 같이 공유는 하지 않는다. 
     }
     
 }
