accc250d1ff11e44eef79509f0e202b5d4ed08f9
seojinvely1004 <seojin0124@naver.com>
2022-11-13 13:34:14 +0900

    start self-stury

---
+++ java baseball/baseball/src/main/java/baseball/domain/baseball/Application.java
@@ -0,0 +1,14 @@
+package baseball;
+
+import baseball.domain.Calculator;
+
+public class Application {
+    public static void main(String[] args) {
+        Calculator teacher = new Calculator();
+        System.out.println(teacher.add(1,3));
+        
+   
+        
+    }
+    
+}
