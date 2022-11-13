4795c0cdaa91cdf7245799052c83110922bb6f7a
seojinvely1004 <seojin0124@naver.com>
2022-11-13 15:01:19 +0900

    feat(baseball): create skelton code

--- java baseball/baseball/src/main/java/baseball/domain/baseball/baseball/baseball/Application.java
+++ java baseball/baseball/src/main/java/baseball/domain/baseball/baseball/baseball/Application.java
@@ -1,11 +1,13 @@
 package baseball;
+/*객체 지향 프로그래밍 
+*1. 기능을 가지고 있는 클래스를 인스턴스화(= 객체, object) 한다. 
+*2. 필요한 기능을 각 인스턴스가 역할에 맞게 수행하게 한다.(의인화) 즉, 기능별로 나눈다. 
+*3. 그 결과를 종합한다( 종합하여 하나의 프로그램을 동작시키게 한다. )
+ */
 
 public class Application {
     public static void main(String[] args) {
-        int[] computer = {1,2,3};
-        int[] player = {1,2,3};
-
-        "1볼 1스트라이크" = match(computer,player);
+        
         
     }
     
