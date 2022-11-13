17c012b2d8670145dd4049508e31639e4c4ad5e8
seojinvely1004 <seojin0124@naver.com>
2022-11-13 17:29:21 +0900

    feat(baseball): 기능 완성

--- java baseball/baseball/src/main/java/baseball/domain/baseball/baseball/baseball/Application.java
+++ java baseball/baseball/src/main/java/baseball/domain/baseball/baseball/baseball/Application.java
@@ -1,6 +1,8 @@
 package baseball;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 import baseball.domain.NumberGenerator;
 /*객체 지향 프로그래밍 
@@ -32,12 +34,36 @@ public class Application {
          //final boolean place = judgement.hasPlace(Arrays.List(7, 8, 9), placeIndex:1, number:8); 
          //System.out.println(place);//true
 
-         final Refree referee = new Referee();
-         final String result = referee.compare(Array.asList(1, 2, 3), Array.asList(1, 2, 3));//referee야 컴퓨터의 숫자와 나의 숫자가 같은지 compare()해줘~ 
-         System.out.println(result);//0볼 3 스트라이크
+         //final Refree referee = new Referee();
+         //final String result = referee.compare(Array.asList(1, 2, 3), Array.asList(1, 2, 3));//referee야 컴퓨터의 숫자와 나의 숫자가 같은지 compare()해줘~ 
+         //System.out.println(result);//0볼 3 스트라이크
 
          //final String result = referee.compare(Array.asList(3, 1, 2), Array.asList(1, 2, 3));
          //System.out.println(result);//3볼 0 스트라이크 
 
+         final NumberGenerator generator = new NumberGenerator();//1. referee에게 주는 숫자는 numberGenerator가 만든 숫자이다.
+         final List<Integer> computer = generator.createRandomNumbers();//generator에게 가져온 숫자로 computer가 RandomNumbers 
+
+         final Refree referee = new Referee();
+
+        //4. result가 0볼 0스트라이크가 되어야 게임이 끝이 난다.
+         String result = "";
+         while (!result.equals("0볼 3스트라이크")) {//0볼 3스트라이크가 아닌동안 반복한다
+            result = fereree.compare(computer, askNumbers());
+            System.out.println(result);
+         }
+         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");     
+    }
+
+    public static List<Integer> askNumbers() {//3.사용자에게 입력을 받기 위해 출력창을 만들어준다
+        System.out.print("숫자를 입력해 주세요 : ");
+        final Scanner scanner = new Scanner(System.in);
+        final String input = scanner.next();
+        
+        List<Integer> numbers = new ArrayList<>(); 
+        for (String number : input.split(regex:"")) {
+            numbers.add(Inteter.valueOf(number));
+            
+        return numbers;
     }
 }
