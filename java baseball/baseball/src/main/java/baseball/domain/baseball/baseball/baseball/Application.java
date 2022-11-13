94a593153503263ab56772abeb416b98a1477c9e
seojinvely1004 <seojin0124@naver.com>
2022-11-13 14:36:43 +0900

    feat: 보통 main()메소드 하나에서한다_객체지향 전

---
+++ java baseball/baseball/src/main/java/baseball/domain/baseball/baseball/baseball/Application.java
@@ -0,0 +1,12 @@
+package baseball;
+
+public class Application {
+    public static void main(String[] args) {
+        int[] computer = {1,2,3};
+        int[] player = {1,2,3};
+
+        "1볼 1스트라이크" = match(computer,player);
+        
+    }
+    
+}
