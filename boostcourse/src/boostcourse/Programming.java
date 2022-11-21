297a39b2b838c19d9f713bef8f3bd2efcf7f3515
seojinvely1004 <seojin0124@naver.com>
2022-11-21 20:26:17 +0900

    feat(boostcourse) : Programming 의미 생각해보기.

---
+++ boostcourse/src/boostcourse/Programming.java
@@ -0,0 +1,17 @@
+package boostcourse;
+
+public class Programming {
+
+	public static void main(String[] args) {
+		//프로그램이란 : 시간에 따라 실행할 작업들의 순서를 의미.
+		//아래의 3줄이 만약 1억줄이라면? > Java라는 언어로 기계에게 위임해서 자동화 하자. 
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		//프로그래밍이란 무엇인지 생각해보기 
+		//특정 목적을 달성하기 위해 수식 등을 컴퓨터가 이해 할 수 있도록 순서화(알고리즘)시키고  프로그래밍 언어 고유의 특징으로 코드화 시켜 고쳐 적어서 자동화를 달성하는 것.
+		
+
+	}
+
+}
