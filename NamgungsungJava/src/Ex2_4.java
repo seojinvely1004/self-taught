e5e9233e8ddeecfc64169817725fef77657ff6c2
seojinvely1004 <seojin0124@naver.com>
2023-02-01 15:52:35 +0900

    feat(Java) : variable

---
+++ NamgungsungJava/src/Ex2_4.java
@@ -0,0 +1,15 @@
+
+public class Ex2_4 {
+	public static void main(String args[]){
+		int x = 100;
+		double pi=3.14;
+		char ch='a';
+		String str = "abc";
+		System.out.println(x);
+		System.out.println(pi);
+		System.out.println(ch);
+		System.out.println(str);
+		
+	}
+
+}
