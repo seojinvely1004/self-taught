78fc9aaa815aa560deb5d5bd746940b34a8e1ef2
seojinvely1004 <seojin0124@naver.com>
2023-02-04 20:05:26 +0900

    feat(Java) : 사칙 연산자 형변환적용

---
+++ NamgungsungJava/src/Ex3_6.java
@@ -0,0 +1,16 @@
+
+public class Ex3_6 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 4;
+		
+		System.out.printf("%d + %d = %d%n", a, b, a + b);//10 + 4 = 14
+		System.out.printf("%d - %d = %d%n", a, b, a - b);//10 - 4 = 6
+		System.out.printf("%d * %d = %d%n", a, b, a * b);//10 * 4 = 40
+		System.out.printf("%d / %d = %d%n", a, b, a / b);//10 / 4 = 2
+		System.out.printf("%d / %f = %f%n", a, (float)b, a + (float)b);//10 / 4.000000 = 14.000000
+
+	}
+
+}
