6c5000231bbb3a77265abbc5fb494a73e84e77fc
seojinvely1004 <seojin0124@naver.com>
2023-02-05 15:22:19 +0900

    feat(Java) : for문

---
+++ NamgungsungJava/src/Ex4_9.java
@@ -0,0 +1,17 @@
+
+public class Ex4_9 {
+
+	public static void main(String[] args) {
+		//반복 횟수를 알고 있을 때는 for문을, 그렇지 않을 때는 while문을 사용한다.
+		for(int i=1; i<=5; i++) 
+			System.out.println(i);
+			
+			for(int i = 1; i <=5; i++)
+				System.out.print(i);//print()사용하여 가로로 출력
+			
+			System.out.println();
+		
+
+	}
+
+}
