ec8080932627ef18c567a31f6cf387843bcaea35
seojinvely1004 <seojin0124@naver.com>
2023-02-05 14:31:30 +0900

    feat(Java):Math.round()로반올림

---
+++ NamgungsungJava/src/Ex3_11.java
@@ -0,0 +1,11 @@
+
+public class Ex3_11 {
+
+	public static void main(String[] args) {
+		double pi = 3.141592;
+		double shorPi = Math.round(pi * 1000) / 1000.0;
+		System.out.println(shorPi);//3.142
+
+	}
+
+}
