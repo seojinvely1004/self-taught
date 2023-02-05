d4a19862dd523c477897291b4c28c2c43ad5b428
seojinvely1004 <seojin0124@naver.com>
2023-02-05 14:33:56 +0900

    feat(Java) : 나머지 연산자

---
+++ NamgungsungJava/src/Ex3_12.java
@@ -0,0 +1,11 @@
+
+public class Ex3_12 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 8;
+		System.out.printf("%d를 %d로 나누면 %n", x, y);//10를 8로 나누면 
+		System.out.printf("몫음 %d이고, 나머지는 %d입니다.%n", x/y, x%y);//몫음 1이고, 나머지는 2입니다.
+	}
+
+}
