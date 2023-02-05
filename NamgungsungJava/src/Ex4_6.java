0225856bc22e5c33641798d7bc6d9efd7adef986
seojinvely1004 <seojin0124@naver.com>
2023-02-05 15:18:06 +0900

    feat(docs) : switch문의 제약조건

--- NamgungsungJava/src/Ex4_6.java
+++ NamgungsungJava/src/Ex4_6.java
@@ -8,10 +8,10 @@ public class Ex4_6 {
 		Scanner scanner = new Scanner(System.in);
 		int month = scanner.nextInt();//화면을 통해 입력받은 숫자를 month에 저장
 		
-		switch(month) {
+		switch(month) {//switch문의 조건식 결과는 정수 또는 문자열이어야한다.
 		case 3:
 			case 4:
-				case 5:
+				case 5://case문은 정수, 상수,문자열만 가능하며 중복되지 않아야 한다 
 					System.out.println("현재의 계절은 봄입니다.");
 					break;
 				case 6: case 7: case 8:
