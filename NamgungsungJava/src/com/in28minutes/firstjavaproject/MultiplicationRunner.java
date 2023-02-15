0f39a4b9e80d329b73581762f9df62b5ecb12b58
seojinvely1004 <seojin0124@naver.com>
2023-02-15 17:15:42 +0900

    feat(Java) : 5단 출력

---
+++ NamgungsungJava/src/com/in28minutes/firstjavaproject/MultiplicationRunner.java
@@ -0,0 +1,9 @@
+package com.in28minutes.firstjavaproject;
+
+public class MultiplicationRunner {
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable();
+		table.print();
+	}
+
+}
