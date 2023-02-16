556e022dcc41608c55893e2c32d62ffaad12df81
seojinvely1004 <seojin0124@naver.com>
2023-02-16 16:39:19 +0900

    feat(Java) : refactoring 1

--- NamgungsungJava/src/com/in28minutes/firstjavaproject/MultiplicationRunner.java
+++ NamgungsungJava/src/com/in28minutes/firstjavaproject/MultiplicationRunner.java
@@ -4,8 +4,8 @@ public class MultiplicationRunner {
 	public static void main(String[] args) {
 		MultiplicationTable table = new MultiplicationTable();
 		//table.print();
-		//table.print(6);
-		table.print(6, 11, 20);
+		table.print(6);
+		// table.print(6, 11, 20);
 	}
 
 }
--- NamgungsungJava/src/com/in28minutes/firstjavaproject/MultiplicationTable.java
+++ NamgungsungJava/src/com/in28minutes/firstjavaproject/MultiplicationTable.java
@@ -2,17 +2,19 @@ package com.in28minutes.firstjavaproject;
 
 public class MultiplicationTable {
 
-	//5*1 = 5 
-	//5 * 10 = 50
 	void print() {
-		for(int i = 1; i <=10; i ++) {
-			System.out.printf("%d * %d = %d", 5, i, 5 * i ).println();
+		print();
+		for (int i = 1; i <= 10; i++) {
+			System.out.printf("%d * %d = %d", 5, i, 5 * i).println();
 		}
+
 	}
 	void print(int table) {
-		for(int i = 1; i<=10; i++) {
-			System.out.printf("%d * %d = %d", table, i , table * i).println();
-		}
+		print(table, 1, 10);
+		/*
+		 * for(int i = 1; i<=10; i++) { System.out.printf("%d * %d = %d", table, i ,
+		 * table * i).println(); }
+		 */
 	}
 	void print(int table, int from,int to ) {
 		for(int i = from; i<=to; i++) {
