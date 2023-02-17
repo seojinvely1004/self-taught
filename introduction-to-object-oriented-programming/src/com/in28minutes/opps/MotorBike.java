1f98da1ca9cb5a27ff671dbb46272e550b8dde3f
seojinvely1004 <seojin0124@naver.com>
2023-02-17 16:06:53 +0900

    feat(Java) : 객체 상태의 변화 주고 동작 흐름 보기

--- introduction-to-object-oriented-programming/src/com/in28minutes/opps/MotorBike.java
+++ introduction-to-object-oriented-programming/src/com/in28minutes/opps/MotorBike.java
@@ -1,6 +1,9 @@
 package com.in28minutes.opps;
 
 public class MotorBike {
+
+	int speed;// 인스턴스변수는 개체로부터 참조될 수 없다
+
 	void start() {
 		System.out.println("Bike Start");
 
--- introduction-to-object-oriented-programming/src/com/in28minutes/opps/MotorBikeRunner.java
+++ introduction-to-object-oriented-programming/src/com/in28minutes/opps/MotorBikeRunner.java
@@ -9,6 +9,13 @@ public class MotorBikeRunner {
 		ducati.start();
 		honda.start();
 
+		// MotorBike.speed; ERROR)인스턴스변수는 개체로부터 참조될 수 없다
+		ducati.speed = 100;
+		honda.speed = 80;
+
+		ducati.speed = 20;
+		honda.speed = 0;
+
 	}
 
 }
